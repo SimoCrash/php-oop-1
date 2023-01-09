e712420468729ecc3d840f051bd22ce0b8677bef
SimoCrash <simone.crescenzi@hotmail.com>
2023-01-09 18:56:34 +0100

    es completo

--- index.php
+++ index.php
@@ -4,13 +4,12 @@
         public $year;
         public $genre;
         public $eta;
-
-        public function __construct($title, $year, $genre, $eta)
+        public function __construct($title, $year, $genre)
         {
             $this->title = $title;
             $this->year = $year;
             $this->genre = $genre;
-            $this->eta = $eta;
+            $this->setEta();
         }
 
         public function setEta(){
@@ -21,10 +20,36 @@
             return $this->eta;
         }
     }
+	$matrix = new Movie('The Matrix', 1999, 'Sci-Fi');
+    $up = new Movie ('Up', 2009, 'animation');
+    $non_aprire_quella_porta = new Movie ('Non aprire quella porta', 2006, 'horror');
+    $parto_col_folle = new Movie ('Parto col folle', 2010, 'comedy');
+	
+    $movie_matrix = array("Title: " . $matrix->title, "Year: " . $matrix->year, "Genre: " . $matrix->genre, "Age: " . $matrix->getEta());
+    for ($i = 0; $i < count($movie_matrix); $i++){
+        echo $movie_matrix[$i] . "<br>";
+    }
+    echo "<br>";
 
-    $up = new Movie ('Up', 2009, 'animation', $eta_up = $up->getEta());
-    $non_aprire_quella_porta = new Movie ('Non aprire quella porta', 2006, 'horror', getEta());
-    $parto_col_folle = new Movie ('Parto col folle', 2010, 'comedy', getEta());
-    var_dump($up, $non_aprire_quella_porta, $parto_col_folle);
-    echo $up, $non_aprire_quella_porta, $parto_col_folle;
+    $movie_up = array("Title: " . $up->title, "Year: " . $up->year, "Genre: " . $up->genre, "Age: " . $up->getEta());
+    for ($i = 0; $i < count($movie_up); $i++){
+        echo $movie_up[$i] . "<br>";
+    }
+    echo "<br>";
+
+    $movie_non_aprire_quella_porta = array("Title: " . $non_aprire_quella_porta->title, "Year: " . $non_aprire_quella_porta->year, "Genre: " . $non_aprire_quella_porta->genre, "Age: " . $non_aprire_quella_porta->getEta());
+    for ($i = 0; $i < count($movie_non_aprire_quella_porta); $i++){
+        echo $movie_non_aprire_quella_porta[$i] . "<br>";
+    }
+    echo "<br>";
+
+    $movie_parto_col_folle = array("Title: " . $parto_col_folle->title, "Year: " . $parto_col_folle->year, "Genre: " . $parto_col_folle->genre, "Age: " . $parto_col_folle->getEta());
+    for ($i = 0; $i < count($movie_parto_col_folle); $i++){
+        echo $movie_parto_col_folle[$i] . "<br>";
+    }
+
+
+	// echo "Titolo: ". $matrix->title;
+    // echo "Anno: ". $matrix->year . "(" . $matrix->getEta() . " anni)";
+    // echo "Genere: ". $matrix->genre;
 ?>
