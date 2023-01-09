73c01c45c5a3a30eb5340b7575e98aa08918cff8
SimoCrash <simone.crescenzi@hotmail.com>
2023-01-09 17:06:08 +0100

    push per hotwell

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
+    class Movie {
+        public $title;
+        public $year;
+        public $genre;
+        public $eta;
+
+        public function __construct($title, $year, $genre, getEta())
+        {
+            $this->title = $title;
+            $this->year = $year;
+            $this->genre = $genre;
+        }
+
+        public function setEta(){
+            $this->eta = 2023 - $this->year;
+        }
+
+        public function getEta(){
+            return $this->eta;
+        }
+    }
+
+    $up = new Movie('Up', 2009, 'animation', getEta());
+    $non_aprire_quella_porta = new Movie ('Non aprire quella porta', 2006, 'horror', getEta());
+    $parto_col_folle = new Movie ('Parto col folle', 2010, 'comedy', getEta());
+    var_dump($up, $non_aprire_quella_porta, $parto_col_folle);
+
 
 ?>
